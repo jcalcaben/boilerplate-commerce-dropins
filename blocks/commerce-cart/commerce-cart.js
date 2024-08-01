041fce5aa562e05a74af7d0b67d6a68b255e0917
jcalcaben <calcaben@adobe.com>
2024-08-01 15:14:08 -0500

    Add Order Summary and Estimate Shipping to Cart page

--- blocks/commerce-cart/commerce-cart.js
+++ blocks/commerce-cart/commerce-cart.js
@@ -2,18 +2,40 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 // Dropin Providers
-import { render as provider } from '@dropins/storefront-cart/render.js';
+import { render as provider } from "@dropins/storefront-cart/render.js";
 
 // Dropin Containers
-import Cart from '@dropins/storefront-cart/containers/Cart.js';
+import Cart from "@dropins/storefront-cart/containers/Cart.js";
+import OrderSummary from "@dropins/storefront-cart/containers/OrderSummary.js";
+import EstimateShipping from "@dropins/storefront-cart/containers/EstimateShipping.js";
 
 export default async function decorate(block) {
   // Initialize Dropins – already initialized in scripts/dropins.js
 
   // Render Containers
   return provider.render(Cart, {
-    routeEmptyCartCTA: () => '/',
+    routeEmptyCartCTA: () => "/",
     routeProduct: (product) => `/products/${product.url.urlKey}/${product.sku}`,
-    routeCheckout: () => '/checkout',
+    routeCheckout: () => "/checkout",
+    slots: {
+      OrderSummary: (ctx) => {
+        const orderSummary = document.createElement("div");
+
+        provider.render(OrderSummary, {
+          routeCheckout: () => "#checkout",
+          slots: {
+            EstimateShipping: (ctx) => {
+              const estimateShippingForm = document.createElement("div");
+
+              provider.render(EstimateShipping)(estimateShippingForm);
+
+              ctx.appendChild(estimateShippingForm);
+            },
+          },
+        })(orderSummary);
+
+        ctx.appendChild(orderSummary);
+      },
+    },
   })(block);
 }
